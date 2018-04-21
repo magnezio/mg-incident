e3789e885bfddef2d3fcbfd547fd206b9f80b27b
Vlad Stepanov <v.a.stpnv@gmail.com>
2018-04-21 23:40:10 +0700

    Fix db username

--- config/development.py
+++ config/development.py
@@ -2,5 +2,5 @@ DEBUG = True
 
 SECRET_KEY = 'how much is the fish?'
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://mgincident_webapp:masterkey@localhost/mgincident_dev'
+SQLALCHEMY_DATABASE_URI = 'postgresql://mgincident_app:masterkey@localhost/mgincident_dev'
 SQLALCHEMY_ECHO = True
