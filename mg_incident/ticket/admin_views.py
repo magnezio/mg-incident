353162614207defc94e395a499fff393b12e2614
Vlad Stepanov <v.a.stpnv@gmail.com>
2018-04-22 11:47:41 +0700

    Add based model views to tickets

---
+++ mg_incident/ticket/admin_views.py
@@ -0,0 +1,21 @@
+from flask_admin.contrib.sqla import ModelView
+
+from mg_incident import db, admin
+from mg_incident.ticket import models
+
+
+class TicketView(ModelView):
+    pass
+
+
+class StatusView(ModelView):
+    pass
+
+
+class TicketStatusView(ModelView):
+    pass
+
+
+admin.add_view(TicketView(models.Ticket, db.session))
+admin.add_view(StatusView(models.Status, db.session))
+admin.add_view(TicketStatusView(models.TicketStatus, db.session))
