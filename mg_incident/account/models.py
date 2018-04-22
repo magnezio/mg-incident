e1681c9b42c4b7d2ced20afd6c7740498dd41942
Vlad Stepanov <v.a.stpnv@gmail.com>
2018-04-22 09:06:31 +0700

    Add User and Role models

--- mg_incident/account/models.py
+++ mg_incident/account/models.py
@@ -1,4 +1,8 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+import datetime
+
+from sqlalchemy import Column, PrimaryKeyConstraint, ForeignKey, \
+        Integer, String, Boolean, DateTime
+
 from sqlalchemy.orm import relationship, backref
 
 from flask_security import (
@@ -8,15 +12,48 @@ from flask_security import (
 from mg_incident import db
 
 
-class AppUser(db.Model, UserMixin):
-    __tablename__ = 'appuser'
-    id = Column(Integer, primary_key=True)
-    nickname = Column(String(50), unique=True, nullable=False)
-    email = Column(String(50), unique=True, nullable=False)
+appuser_approle = db.Table(
+    'appuser_approle',
+    Column(
+        'appuser_id', Integer, ForeignKey('appuser.id', ondelete='SET NULL')
+    ),
+    Column(
+        'approle_id', Integer, ForeignKey('approle.id', ondelete='SET NULL')
+    ),
+    PrimaryKeyConstraint('appuser_id', 'approle_id')
+)
 
 
 class AppRole(db.Model, RoleMixin):
     __tablename__ = 'approle'
     id = Column(Integer, primary_key=True)
-    name = Column(String(255), unique=True, nullable=False)
-    descr = Column(String(255))
+    name = Column(String(50), unique=True, nullable=False)
+    description = Column(String(255), unique=True, nullable=False)
+    users = relationship(
+        'AppUser',
+        uselist=True,
+        backref=backref('roles', lazy='dynamic'),
+        lazy='dynamic',
+        secondary=appuser_approle
+    )
+
+    def __repr__(self):
+        return self.name
+
+
+class AppUser(db.Model, UserMixin):
+    __tablename__ = 'appuser'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    active = Column(Boolean)
+    password = Column(String(255))
+    last_login_at = Column(DateTime())
+    current_login_at = Column(DateTime())
+    last_login_ip = Column(String(100))
+    current_login_ip = Column(String(100))
+    login_count = Column(Integer)
+    confirmed_at = Column(DateTime())
+
+    def __repr__(self):
+        return "{} <{}>".format(self.username, self.email)
