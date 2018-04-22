615419374c827033dcca312558aa14d9afc019fa
Vlad Stepanov <v.a.stpnv@gmail.com>
2018-04-22 20:36:19 +0700

    Commom structure changes

--- mg_incident/account/core.py
+++
@@ -1,7 +0,0 @@
-# from flask import Blueprint
-
-
-# bp = Blueprint('account', __name__)
-
-from mg_incident.account import models
-from mg_incident.account import admin_views
--- mg_incident/ticket/core.py
+++
@@ -1,7 +0,0 @@
-# from flask import Blueprint
-
-
-# bp = Blueprint('ticket', __name__)
-
-from mg_incident.ticket import models
-from mg_incident.ticket import admin_views
