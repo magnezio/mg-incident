9f7d8b3ab1e2bbd0c5926e1e4ecbdddffec69a33
Vlad Stepanov <v.a.stpnv@gmail.com>
2018-04-23 18:24:24 +0700

    Update config

--- config/development.py
+++ config/development.py
@@ -3,6 +3,7 @@ DEBUG = True
 SECRET_KEY = 'how much is the fish?'
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://mgincident_app:masterkey@localhost/mgincident_dev'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = True
 
 SECURITY_PASSWORD_SALT = 'now much is the fish?'
