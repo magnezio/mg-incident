f9e1966fcfc24e3cab3dc8919a6509e248b8d285
vlad_stepanoff <v.a.stpnv@gmail.com>
2018-04-23 12:46:08 +0700

    Update default settings

--- config/default.py
+++ config/default.py
@@ -3,14 +3,30 @@ import os
 
 DEBUG = False
 
-BABEL_DEFAULT_LOCALE = os.environ.get('MGI_LOCALE', 'ru')
-BABEL_DEFAULT_TIMEZONE = os.environ.get('MGI_TIMEZONE', 'UTC')
 
-DB_USER = os.environ.get('MGI_DB_USER', 'mgincident_app')
-DB_PASSWORD = os.environ.get('MGI_DB_PASSWORD')
-DB_HOST = os.environ.get('MGI_DB_HOST', 'localhost')
-DB_NAME = os.environ.get('MGI_DB_NAME', 'mgincident')
+DB_USER = os.environ.get("MGI_DB_USER", "mgincident_app")
+DB_PASSWORD = os.environ.get("MGI_DB_PASSWORD")
+DB_HOST = os.environ.get("MGI_DB_HOST", "localhost")
+DB_NAME = os.environ.get("MGI_DB_NAME", "mgincident")
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}/{}'.format(
+
+SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(
     DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 )
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+SECURITY_URL_PREFIX = "/"
+SECURITY_PASSWORD_HASH = "pbkdf2_sha512"
+SECURITY_PASSWORD_SALT = "KJbGHUIFklaihHOIH809HGbldSsj8"
+
+SECURITY_LOGIN_URL = "/login/"
+SECURITY_LOGOUT_URL = "/logout/"
+SECURITY_REGISTER_URL = "/register/"
+
+SECURITY_POST_LOGIN_VIEW = "/"
+SECURITY_POST_LOGOUT_VIEW = "/"
+SECURITY_POST_REGISTER_VIEW = "/"
+
+SECURITY_REGISTERABLE = False
+SECURITY_SEND_REGISTER_EMAIL = False
