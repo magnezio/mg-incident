c3715f45f471b7b6e24665d0d21e1ab5a3a7d8e4
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-04-25 15:50:51 +0300

    issue 8. cleanup filter for tickets FA view

--- mg_incident/ticket/admin_views.py
+++ mg_incident/ticket/admin_views.py
@@ -1,42 +1,10 @@
 from flask_admin.contrib.sqla import ModelView
-from flask_admin.contrib.sqla.filters import BaseSQLAFilter
 from mg_incident import db, admin
 from mg_incident.auth import UserRequiredMixin
 from mg_incident.ticket import models
 
 
-class FilterCreatedBy(BaseSQLAFilter):
-    def __init__(self, column, name, options=None, data_type=None):
-        def get_users():
-            users = []
-            try:
-                # TODO Problem are here.
-                # TODO UPD. Problem partly resolved. This code for future fix. Mystery!
-                # "No application found. Either work inside a view function or push an application context."
-                # I tried use it with current_app.app_context but unsuccessfully
-                # users = models.Ticket.query.all()
-                pass
-            except Exception as e:
-                pass
-            return [(user.id, str(user)) for user in users]
-
-        super(FilterCreatedBy, self).__init__(column, name, get_users(), data_type)
-
-    def apply(self, query, value, alias=None):
-        return query.filter(self.column == value)
-
-    def operation(self):
-        from flask_admin.babel import gettext
-        return gettext('equals')
-
-
 class TicketView(UserRequiredMixin, ModelView):
-    # TODO Options should be generated automatically, but now its problem to make query to db from current view
-    # TODO UPD. Problem partly resolved. This code for future fix. Mystery!
-    # column_filters = [
-    #     FilterCreatedBy(models.Ticket.created_by_id, 'Created By',
-    #                     options=[('1', 'User With ID 1'), ('2', 'User with ID 2, etc..')])
-    # ]
     column_filters = ('created_by.username', 'assigned_by.username', 'assigned_to.username',)
 
 
