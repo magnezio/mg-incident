c01948ab04d9ff9ead58a2981b651fb59275d6cc
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-04-26 18:52:19 +0300

    Ticket self-referential. Prohibit delete tickets that have child records. #18

--- mg_incident/ticket/admin_views.py
+++ mg_incident/ticket/admin_views.py
@@ -1,4 +1,7 @@
+from flask import flash
+from flask_admin.babel import gettext
 from flask_admin.contrib.sqla import ModelView
+from wtforms import ValidationError
 
 from mg_incident import db, admin
 from mg_incident.auth import UserRequiredMixin
@@ -6,7 +9,38 @@ from mg_incident.ticket import models
 
 
 class TicketView(UserRequiredMixin, ModelView):
-    pass
+    form_columns = ('name', 'description', 'parent', 'children', 'ticket_statuses',
+                    'created_at', 'created_by', 'assigned_by', 'assigned_to')
+
+    def on_model_delete(self, model):
+        if model.children:
+            raise ValidationError("You can't deleting tickets that have child records")
+
+    def delete_model(self, model):
+        """
+            Delete model.
+            :param model:
+                Model to delete
+        """
+        try:
+            self.on_model_delete(model)
+            if not model.children:
+                self.session.flush()
+                self.session.delete(model)
+                self.session.commit()
+            if model.children:
+                return False
+        except Exception as ex:
+            if not self.handle_view_exception(ex):
+                flash(gettext('Failed to delete record. %(error)s', error=str(ex)), 'error')
+
+            self.session.rollback()
+
+            return False
+        else:
+            self.after_model_delete(model)
+
+        return True
 
 
 class StatusView(UserRequiredMixin, ModelView):
@@ -14,7 +48,7 @@ class StatusView(UserRequiredMixin, ModelView):
 
 
 class TicketStatusView(UserRequiredMixin, ModelView):
-    form_columns = ('ticket', 'status', 'description', 'created_by', )
+    form_columns = ('ticket', 'status', 'description', 'created_by',)
 
 
 admin.add_view(TicketView(models.Ticket, db.session))
--- mg_incident/ticket/models.py
+++ mg_incident/ticket/models.py
@@ -1,14 +1,11 @@
 import datetime
 
-from sqlalchemy import Column, PrimaryKeyConstraint, ForeignKey, \
-        Integer, String, Boolean, DateTime
+from sqlalchemy import Column, ForeignKey, \
+    Integer, String, DateTime
 
 from sqlalchemy.orm import relationship, backref
 
 from mg_incident import db
-
-
-from mg_incident.account.models import AppUser
 
 
 class Status(db.Model):
@@ -25,20 +22,24 @@ class Ticket(db.Model):
     name = Column(String(255), unique=True, nullable=False)
     description = Column(String(255))
     created_by_id = Column(Integer, ForeignKey('appuser.id', ondelete='SET NULL'),
-                            nullable=False)
+                           nullable=False)
     assigned_by_id = Column(Integer, ForeignKey('appuser.id', ondelete='SET NULL'))
     assigned_to_id = Column(Integer, ForeignKey('appuser.id', ondelete='SET NULL'))
     ticket_statuses = relationship('TicketStatus', backref='ticket')
     created_at = Column(DateTime, default=datetime.datetime.now, nullable=False)
+
+    # self-referential
+    parent_id = Column(Integer, ForeignKey('ticket.id'))
+    children = relationship('Ticket', backref=backref('parent', remote_side=[id]))
 
 
 class TicketStatus(db.Model):
     __tablename__ = 'ticket_status'
     id = Column(Integer, primary_key=True)
     description = Column(String(255))
-    ticket_id = Column(Integer, ForeignKey('ticket.id', on_delete='CASCADE'), 
+    ticket_id = Column(Integer, ForeignKey('ticket.id', on_delete='CASCADE'),
                        nullable=False)
-    status_id = Column(Integer, ForeignKey('status.id', on_delete='SET NULL'), 
+    status_id = Column(Integer, ForeignKey('status.id', on_delete='SET NULL'),
                        nullable=False)
     created_by_id = Column(Integer, ForeignKey('appuser.id', ondelete='SET NULL'),
                            nullable=False)
