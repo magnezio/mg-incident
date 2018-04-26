846c6f2eaf84d636cbd20c4d187662b4834064fa
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-04-26 17:26:42 +0300

    Predefined roles: users, workers, managers, admininstrators. Prohibit removal or change of a predefined role. #22

--- mg_incident/account/admin_views.py
+++ mg_incident/account/admin_views.py
@@ -1,23 +1,62 @@
+from flask import flash
+from flask_admin.babel import gettext
 from flask_admin.contrib.sqla import ModelView
+from wtforms import ValidationError
 
 from mg_incident import db
 from mg_incident import admin
 
-from mg_incident.auth import UserRequiredMixin, AdminRequiredMixin
+from mg_incident.auth import AdminRequiredMixin
 from . import models
 
 
 class AppUser(AdminRequiredMixin, ModelView):
-    column_list = ['username', 'email', 'active', 'last_login_at', \
-        'last_login_ip', 'login_count', ]
+    column_list = ['username', 'email', 'active', 'last_login_at',
+                   'last_login_ip', 'login_count', ]
     column_searchable_list = ['username', 'email', ]
     column_filters = ['active', 'roles', ]
     form_excluded_columns = ['password', ]
 
 
 class AppRole(AdminRequiredMixin, ModelView):
-    column_list = ['name', 'description', ]
+    column_list = ['name', 'description', 'predefined']
     column_searchable_list = ['name', ]
+    form_excluded_columns = ['predefined']
+
+    def on_model_delete(self, model):
+        if model.predefined:
+            raise ValidationError('Predefined role can not be deleted.')
+
+    def on_model_change(self, form, model, is_created):
+        if model.predefined:
+            raise ValidationError('Predefined role can not be changed.')
+
+    def delete_model(self, model):
+        """
+            Delete model.
+
+            :param model:
+                Model to delete
+        """
+        try:
+            self.on_model_delete(model)
+            if not model.predefined:
+                self.session.flush()
+                self.session.delete(model)
+                self.session.commit()
+            if model.predefined:
+                return False
+        except Exception as ex:
+            if not self.handle_view_exception(ex):
+                flash(gettext('Failed to delete record. %(error)s', error=str(ex)), 'error')
+
+            self.session.rollback()
+
+            return False
+        else:
+            self.after_model_delete(model)
+
+        return True
 
 
 admin.add_view(AppUser(models.AppUser, db.session))
--- run.py
+++ run.py
@@ -1,7 +1,42 @@
+import click
+
 from mg_incident import create_app
 
 app = create_app()
 
 
+@app.cli.command()
+def insert_predefined_roles_for_users():
+    from mg_incident.account.models import AppRole
+    from mg_incident import db
+
+    user = AppRole(name='user',
+                   description='Can set status only for his ticket.',
+                   predefined=True)
+
+    worker = AppRole(name='worker',
+                     description='Can set status only for tickets assigned to it.',
+                     predefined=True)
+
+    manager = AppRole(name='manager',
+                      description='Can set status for all type of tickets.',
+                      predefined=True)
+
+    admin = AppRole(name='admin',
+                    description='Full access.',
+                    predefined=True)
+
+    roles = [user, worker, manager, admin]
+
+    report = []
+    with app.app_context():
+        for r in roles:
+            _r = AppRole.query.filter(AppRole.name == r.name).first()
+            if not _r:
+                db.session.add(r)
+                report.append(r.name)
+        db.session.commit()
+
+    click.echo('Insert roles: ' + str(report))
 
 # FLASK_APP=run.py flask run
