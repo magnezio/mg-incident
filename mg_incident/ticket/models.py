474c38eb7bb0f42b706466d1db32748380185bb3
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-04-26 12:24:07 +0300

    Implemented auto set created_by field at TicketStatus FA view #34

--- mg_incident/ticket/models.py
+++ mg_incident/ticket/models.py
@@ -1,14 +1,11 @@
 import datetime
 
-from sqlalchemy import Column, PrimaryKeyConstraint, ForeignKey, \
-        Integer, String, Boolean, DateTime
+from sqlalchemy import Column, ForeignKey, \
+    Integer, String, DateTime
 
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 
 from mg_incident import db
-
-
-from mg_incident.account.models import AppUser
 
 
 class Status(db.Model):
@@ -18,6 +15,12 @@ class Status(db.Model):
     description = Column(String(255))
     ticket_statuses = relationship('TicketStatus', backref='status')
 
+    def __repr__(self):
+        description = ' '
+        if self.description:
+            description = ' (' + str(self.description) + ') '
+        return self.name + description
+
 
 class Ticket(db.Model):
     __tablename__ = 'ticket'
@@ -25,20 +28,32 @@ class Ticket(db.Model):
     name = Column(String(255), unique=True, nullable=False)
     description = Column(String(255))
     created_by_id = Column(Integer, ForeignKey('appuser.id', ondelete='SET NULL'),
-                            nullable=False)
+                           nullable=False)
     assigned_by_id = Column(Integer, ForeignKey('appuser.id', ondelete='SET NULL'))
     assigned_to_id = Column(Integer, ForeignKey('appuser.id', ondelete='SET NULL'))
     ticket_statuses = relationship('TicketStatus', backref='ticket')
     created_at = Column(DateTime, default=datetime.datetime.now, nullable=False)
+
+    def __repr__(self):
+        description = ' '
+        if self.description:
+            description = ' (' + str(self.description) + ') '
+        return '<id: {}> {} {} <created by: >'.format(self.id, self.name, description, self.created_by.username)
 
 
 class TicketStatus(db.Model):
     __tablename__ = 'ticket_status'
     id = Column(Integer, primary_key=True)
     description = Column(String(255))
-    ticket_id = Column(Integer, ForeignKey('ticket.id', on_delete='CASCADE'), 
+    ticket_id = Column(Integer, ForeignKey('ticket.id', on_delete='CASCADE'),
                        nullable=False)
-    status_id = Column(Integer, ForeignKey('status.id', on_delete='SET NULL'), 
+    status_id = Column(Integer, ForeignKey('status.id', on_delete='SET NULL'),
                        nullable=False)
     created_by_id = Column(Integer, ForeignKey('appuser.id', ondelete='SET NULL'),
                            nullable=False)
+
+    def __repr__(self):
+        description = ' '
+        if self.description:
+            description = ' (' + str(self.description) + ') '
+        return '{} {}'.format(self.id, description)
