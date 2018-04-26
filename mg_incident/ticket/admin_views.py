004454b768bc4bcd4f0097bc2ba297c7872d2b6c
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-04-26 16:41:45 +0300

    Prohibit removal of a predefined ticket status #23

--- mg_incident/ticket/admin_views.py
+++ mg_incident/ticket/admin_views.py
@@ -1,4 +1,7 @@
+from flask import flash
+from flask_admin.babel import gettext
 from flask_admin.contrib.sqla import ModelView
+from wtforms import ValidationError
 
 from mg_incident import db, admin
 from mg_incident.auth import UserRequiredMixin
@@ -10,6 +13,37 @@ class TicketView(UserRequiredMixin, ModelView):
 
 
 class StatusView(UserRequiredMixin, ModelView):
+    def on_model_delete(self, model):
+        if model.predefined:
+            raise ValidationError('Predefined status can not be deleted.')
+
+    def delete_model(self, model):
+        """
+            Delete model.
+
+            :param model:
+                Model to delete
+        """
+        try:
+            self.on_model_delete(model)
+            if not model.predefined:
+                self.session.flush()
+                self.session.delete(model)
+                self.session.commit()
+            if model.predefined:
+                return False
+        except Exception as ex:
+            if not self.handle_view_exception(ex):
+                flash(gettext('Failed to delete record. %(error)s', error=str(ex)), 'error')
+
+            self.session.rollback()
+
+            return False
+        else:
+            self.after_model_delete(model)
+
+        return True
+
     form_excluded_columns = ('predefined',)
 
 
