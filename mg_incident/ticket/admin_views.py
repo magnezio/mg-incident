42c0c955985f3c8be190765293f56637bbc6f142
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-04-27 09:51:25 +0300

    Implemented model for ticket_status, ticket_status_tracking table (refractoring). #10 #11

--- mg_incident/ticket/admin_views.py
+++ mg_incident/ticket/admin_views.py
@@ -9,14 +9,14 @@ class TicketView(UserRequiredMixin, ModelView):
     pass
 
 
-class StatusView(UserRequiredMixin, ModelView):
+class TicketStatusView(UserRequiredMixin, ModelView):
     pass
 
 
-class TicketStatusView(UserRequiredMixin, ModelView):
-    form_columns = ('ticket', 'status', 'description', 'created_by', )
+class TicketStatusTrackingView(UserRequiredMixin, ModelView):
+    form_columns = ('ticket', 'ticket_status', 'description', 'created_by', 'created_at')
 
 
 admin.add_view(TicketView(models.Ticket, db.session))
-admin.add_view(StatusView(models.Status, db.session))
 admin.add_view(TicketStatusView(models.TicketStatus, db.session))
+admin.add_view(TicketStatusTrackingView(models.TicketStatusTracking, db.session))
