1b6f6530bf9a1f1b7a0eaccbb1fd89eca3a0f37c
Vlad Stepanov <v.a.stpnv@gmail.com>
2018-04-29 20:23:43 +0700

    #20 Add ticket status view module to package

--- mg_incident/admin_views/__init__.py
+++ mg_incident/admin_views/__init__.py
@@ -1,9 +1,10 @@
 from mg_incident.admin_views.account import AppRoleView, AppUserView
-from mg_incident.admin_views.ticket import TicketView
+from mg_incident.admin_views.ticket import TicketView, TicketStatusView
 
 
 __all__ = [
     'AppRoleView',
     'AppUserView',
     'TicketView',
+    'TicketStatusView',
 ]
